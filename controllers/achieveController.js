bbb93d5fb4b2d678be74dfdf7d794c7a4a559fad
AlexxCourter <alex.courter@gmail.com>
2023-02-18 11:32:53 -0500

    Error handling for both endpoints + jdocs added

--- controllers/achieveController.js
+++ controllers/achieveController.js
@@ -1,78 +1,172 @@
+/**
+ * @file Achievement controller handles callbacks for API calls requested by the client related to Achievements or goals in the database.
+ * @author Alexander DK Courter
+ * @version 1.0.2
+ * 
+ * 1.0.2 - wrapped functions in try-catch blocks to handle errors. Separation of error handling managed by conditionals.
+ * 1.0.1 - updated with basic error handling
+ */
+
 const { ObjectId } = require('mongodb');
 const database = require('../db/connector');
 
 const dbName = 'GoalAchievement'; //enter with exact capitalization
 const collName = 'achievements';
 
+
+/**
+ * Returns all objects registered to the Achievements collection.
+ * 
+ * Node Express async function that connects to the database, finds a result (all achievements), then returns it via an HTTP response. The request is forwarded via Express Router from achievements.js
+ * @version 1.0.2
+ * 
+ * @param {*} req The request is forwarded through Express router. See achievements.js
+ * @param {*} res The response is given for a GET request that gets all records
+ */
 const getAllAchievements = async (req, res) => {
-  await database.connectDB();
-  const result = await database.getDb().db(dbName).collection(collName).find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+  try {
+    await database.connectDB();
+    const result = await database.getDb().db(dbName).collection(collName).find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  } catch(e){
+    res.status(500).json(e.message);
+  }
+  
 };
 
+/**
+ * Returns one object registered to the Achievements collection using a specified object ID.
+ * 
+ * Node Express async function that connects to the database, finds a result (one achievement), then returns it via an HTTP response. The request is forwarded via Express Router from achievements.js
+ * @version 1.0.2
+ * 
+ * @param {*} req The request is forwarded through Express router. See achievements.js
+ * @param {*} res The response is given for a GET request that gets one record
+ */
 const getOneAchievement = async (req, res) => {
-  await database.connectDB();
-  const userId = new ObjectId(req.params.id);
-  const result = await database.getDb().db(dbName).collection(collName).find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    await database.connectDB();
+    if(!ObjectId.isValid(req.params.id)){
+      res.status(400).json("You must provide a valid Achievement ID.");
+    }
+    const userId = new ObjectId(req.params.id);
+    const result = await database.getDb().db(dbName).collection(collName).find({ _id: userId });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  } catch(e) {
+    res.status(500).json(e.message);
+  }
+  
 };
 
+
+/**
+ * Creates one object to be registered to the Achievements collection.
+ * 
+ * Node Express async function that connects to the database, inserts a new Achievement record via POST request. The request is forwarded via Express Router from achievements.js
+ * @version 1.0.2
+ * 
+ * @param {*} req The request is forwarded through Express router. See achievements.js
+ * @param {*} res The response acknowledges success with a 201 code
+ */
 const createAchievement = async (req, res) => {
-  const newAchievement = {
-    taskName: req.body.taskName,
-    taskPoints: req.body.points
-  };
-  await database.connectDB();
-  const result = await database.getDb().db(dbName).collection(collName).insertOne(newAchievement);
-  if (result.acknowledged) {
-    res.status(201).json(result);
-  } else {
-    res
-      .status(500)
-      .json(result.error || 'Error: Something went wrong while creating the new achievement.');
+  try{
+    const newAchievement = await postAchieveSchema.validateAsync(req.body);
+
+    await database.connectDB();
+    const result = await database.getDb().db(dbName).collection(collName).insertOne(newAchievement);
+    if (result.acknowledged) {
+      res.status(201).json(result);
+    } else {
+      res
+        .status(500)
+        .json(result.error || 'Error: Something went wrong while creating the new achievement.');
+    }
+  } catch(e){
+    if(e.isJoi === true){
+      res.status(422).json(e.message);
+    } else {
+      res.status(500).json(e.message);
+    }
   }
+  
 };
 
+/**
+ * Updates one object to be registered to the Achievements collection.
+ * 
+ * Node Express async function that connects to the database, inserts a new Achievements record to replace an old equivalent via PUT request. The request is forwarded via Express Router from achievements.js
+ * @version 1.0.2
+ * 
+ * @param {*} req The request is forwarded through Express router. See achievements.js
+ * @param {*} res The response acknowledges that the change was sent to server with 204 code.
+ */
 const updateAchievement = async (req, res) => {
-  await database.connectDB();
-  const userId = new ObjectId(req.params.id);
+  try {
+    await database.connectDB();
 
-  const newAchievement = {
-    taskName: req.body.taskName,
-    taskPoints: req.body.points
-  };
-
-  const result = await database
-    .getDb()
-    .db(dbName)
-    .collection(collName)
-    .replaceOne({ _id: userId }, newAchievement);
-  if (result.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res
-      .status(500)
-      .json(result.error || 'Error: Something went wrong while updating the achievement.');
+    if(!ObjectId.isValid(req.params.id)){
+      res.status(400).json("You must provide a valid User ID.");
+    } else {
+      const userId = new ObjectId(req.params.id);
+  
+      const newAchievement = await postAchieveSchema.validateAsync(req.body);
+    
+      const result = await database
+        .getDb()
+        .db(dbName)
+        .collection(collName)
+        .replaceOne({ _id: userId }, newAchievement);
+      if (result.modifiedCount > 0) {
+        res.status(204).send();
+      } else {
+        res
+          .status(500)
+          .json(result.error || 'Error: Something went wrong while updating the achievement.');
+      }
+    }
+  } catch(e){
+    if(e.isJoi === true){
+      res.status(422).json(e.message);
+    } else {
+      res.status(500).json(e.message);
+    }
   }
+  
+
+  
 };
 
+/**
+ * Deletes one object from the Achievements collection.
+ * 
+ * Node Express async function that connects to the database, deletes an Achievements record via DELETE request. The request is forwarded via Express Router from achievements.js
+ * @version 1.0.2
+ * 
+ * @param {*} req The request is forwarded through Express router. See achievements.js
+ * @param {*} res The response acknowledges success with a 200 code
+ */
 const deleteAchievement = async (req, res) => {
-  await database.connectDB();
-  const userId = new ObjectId(req.params.id);
-  const result = await database.getDb().db(dbName).collection(collName).deleteOne({ _id: userId });
-  if (result.deletedCount > 0) {
-    res.status(200).send();
-  } else {
-    res
-      .status(500)
-      .json(result.error || 'Error: Something went wrong while deleting the achievement.');
+  try{
+    await database.connectDB();
+    const userId = new ObjectId(req.params.id);
+    const result = await database.getDb().db(dbName).collection(collName).deleteOne({ _id: userId });
+    if (result.deletedCount > 0) {
+      res.status(200).send();
+    } else {
+      res
+        .status(500)
+        .json(result.error || 'Error: Something went wrong while deleting the achievement.');
+    }
+  } catch(e){
+    
   }
+  
 };
 
 module.exports = {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,58 @@
+/**
+ * @file User controller handles callbacks for API calls requested by the client related to Users in the database.
+ * @author Alexander DK Courter
+ * @version 1.0.2
+ * 
+ * 1.0.2 - wrapped functions in try-catch blocks to handle errors. Separation of error handling managed by conditionals.
+ * 1.0.1 - updated with basic error handling
+ */
+
+//import necessary modules
 const { ObjectId } = require('mongodb');
 const database = require('../db/connector');
 const {postUserSchema} = require('../validation/validation_schema');
-
+//name of database and collection to be referenced by this controller
 const dbName = 'GoalAchievement'; //enter with exact capitalization
 const collName = 'users';
 
+/**
+ * Returns all objects registered to the Users collection.
+ * 
+ * Node Express async function that connects to the database, finds a result (all users), then returns it via an HTTP response. The request is forwarded via Express Router from user.js
+ * @version 1.0.2
+ * 
+ * @param {*} req The request is forwarded through Express router. See user.js
+ * @param {*} res The response is given for a GET request that gets all records
+ */
 const getAllUsers = async (req, res) => {
-  await database.connectDB();
-  const result = await database.getDb().db(dbName).collection(collName).find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    if(lists.length > 0){
-      res.status(200).json(lists);
-    } else {
-      //error response when an empty dataset is returned
-      res.status(400).json('Could not get Users due to a problem fetching from the database.');
-    }
-    
-  });
+  try{
+    await database.connectDB();
+    const result = await database.getDb().db(dbName).collection(collName).find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      if(lists.length > 0){
+        res.status(200).json(lists);
+      } else {
+        //error response when an empty dataset is returned
+        res.status(400).json('Could not get Users due to a problem fetching from the database.');
+      }
+      
+    });
+  } catch(e) {
+    res.status(500).json(e.message);
+  }
+  
 };
 
+/**
+ * Returns one object registered to the Users collection using a specified object ID.
+ * 
+ * Node Express async function that connects to the database, finds a result (one user), then returns it via an HTTP response. The request is forwarded via Express Router from user.js
+ * @version 1.0.2
+ * 
+ * @param {*} req The request is forwarded through Express router. See user.js
+ * @param {*} res The response is given for a GET request that gets one record
+ */
 const getOneUser = async (req, res) => {
   await database.connectDB();
 
@@ -41,6 +74,15 @@ const getOneUser = async (req, res) => {
   }
 };
 
+/**
+ * Creates one object to be registered to the Users collection.
+ * 
+ * Node Express async function that connects to the database, inserts a new User record via POST request. The request is forwarded via Express Router from user.js
+ * @version 1.0.2
+ * 
+ * @param {*} req The request is forwarded through Express router. See user.js
+ * @param {*} res The response acknowledges success with a 201 code
+ */
 const createUser = async (req, res) => {
   // const newUser = {
   //   username: req.body.userName,
@@ -67,11 +109,18 @@ const createUser = async (req, res) => {
     } else {
       res.status(500).json(e.message);
     }
-    
   }
-  
 };
 
+/**
+ * Updates one object to be registered to the Users collection.
+ * 
+ * Node Express async function that connects to the database, inserts a new User record to replace an old equivalent via PUT request. The request is forwarded via Express Router from user.js
+ * @version 1.0.2
+ * 
+ * @param {*} req The request is forwarded through Express router. See user.js
+ * @param {*} res The response reports success with a 204 code
+ */
 const updateUser = async (req, res) => {
   await database.connectDB();
 
@@ -105,20 +154,34 @@ const updateUser = async (req, res) => {
   }
 };
 
+/**
+ * Deletes one object to be registered to the Users collection.
+ * 
+ * Node Express async function that connects to the database, deletes a User record via DELETE request. The request is forwarded via Express Router from user.js
+ * @version 1.0.2
+ * 
+ * @param {*} req The request is forwarded through Express router. See user.js
+ * @param {*} res The response acknowledges success with a 200 code
+ */
 const deleteUser = async (req, res) => {
-  await database.connectDB();
+  try{
+    await database.connectDB();
 
-  if(!ObjectId.isValid(req.params.id)){
-    res.status(400).json("You must provide a valid User ID.");
-  } else {
-    const userId = new ObjectId(req.params.id);
-    const result = await database.getDb().db(dbName).collection(collName).deleteOne({ _id: userId });
-    if (result.deletedCount > 0) {
-      res.status(200).send();
+    if(!ObjectId.isValid(req.params.id)){
+      res.status(400).json("You must provide a valid User ID.");
     } else {
-      res.status(500).json(result.error || 'Error: Something went wrong while deleting the User.');
+      const userId = new ObjectId(req.params.id);
+      const result = await database.getDb().db(dbName).collection(collName).deleteOne({ _id: userId });
+      if (result.deletedCount > 0) {
+        res.status(200).send();
+      } else {
+        res.status(500).json(result.error || 'Error: Something went wrong while deleting the User.');
+      }
     }
+  } catch(e){
+    res.status(500).json(e.message);
   }
+  
  
 };
 
